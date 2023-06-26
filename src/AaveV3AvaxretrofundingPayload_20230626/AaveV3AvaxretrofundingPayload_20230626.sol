57d42f59198f0e10926471c50827957c47fdcc5f
marczeller <pierarte@gmail.com>
2023-06-26 11:31:04 +0200

    init

--- src/AaveV3AvaxretrofundingPayload_20230626/AaveV3AvaxretrofundingPayload_20230626.sol
+++ src/AaveV3AvaxretrofundingPayload_20230626/AaveV3AvaxretrofundingPayload_20230626.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.10;
+pragma solidity ^0.8.16;
 
 
 import {AaveV3Avalanche, AaveV3AvalancheAssets} from 'aave-address-book/AaveV3Avalanche.sol';
@@ -15,14 +15,21 @@ import {IProposalGenericExecutor} from 'aave-helpers/interfaces/IProposalGeneric
 contract AaveV3AvaxretrofundingPayload is IProposalGenericExecutor {
     address public constant AAVECO_ADDRESS = 0xa8F8E9c54e099c4fADB797f5196E07ce484824aF;
     address public constant aAvaWAVAX = AaveV3AvalancheAssets.WAVAX_A_TOKEN;
+    address public constant avWAVAX = 0xDFE521292EcE2A4f44242efBcD66Bc594CA9714B;
     address public constant aAvaWBTC = AaveV3AvalancheAssets.WBTCe_A_TOKEN;
+    address public constant avWBTC = 0x686bEF2417b6Dc32C50a3cBfbCC3bb60E1e9a15D;
     
-    uint256 public constant AMOUNT_aAvaWAVAX = 9_584_300000000000000000; // $182,723,768
-    uint256 public constant AMOUNT_aAvaWBTC = 1_08000000; // $21,529,26
+    uint256 public constant AMOUNT_aAvaWAVAX = 9_584_300000000000000000;
+    uint256 public constant AMOUNT_avWAVAX = 19_415_700000000000000000;
+
+    uint256 public constant AMOUNT_aAvaWBTC = 1_81000000;
+    uint256 public constant AMOUNT_avWBTC = 1_08000000;
 
 
     function execute() external {
-        AaveV3Avalanche.COLLECTOR_CONTROLLER.transfer(aAvaWAVAX, AAVECO_ADDRESS, AMOUNT_aAvaWAVAX);
-        AaveV3Avalanche.COLLECTOR_CONTROLLER.transfer(aAvaWBTC, AAVECO_ADDRESS, AMOUNT_aAvaWBTC);
+        AaveV3Avalanche.COLLECTOR.transfer(aAvaWAVAX, AAVECO_ADDRESS, AMOUNT_aAvaWAVAX);
+        AaveV3Avalanche.COLLECTOR.transfer(avWAVAX, AAVECO_ADDRESS, AMOUNT_avWAVAX);
+        AaveV3Avalanche.COLLECTOR.transfer(aAvaWBTC, AAVECO_ADDRESS, AMOUNT_aAvaWBTC);
+        AaveV3Avalanche.COLLECTOR.transfer(avWBTC, AAVECO_ADDRESS, AMOUNT_avWBTC);
     }
 }
