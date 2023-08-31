7733ecd66733534dbdc17242de76ada82d88d90c
Rozengarden <github@rozengarden.eu>
2023-08-31 18:02:55 +0200

    Init from generator

---
+++ src/20230831_AaveV3_Pol_SupplyCapLSTs/AaveV3_Polygon_SupplyCapLSTs_20230831.t.sol
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import 'forge-std/Test.sol';
+import {GovHelpers} from 'aave-helpers/GovHelpers.sol';
+import {AaveGovernanceV2} from 'aave-address-book/AaveGovernanceV2.sol';
+import {AaveV3Polygon, AaveV3PolygonAssets} from 'aave-address-book/AaveV3Polygon.sol';
+import {ProtocolV3TestBase, ReserveConfig} from 'aave-helpers/ProtocolV3TestBase.sol';
+import {AaveV3_Polygon_SupplyCapLSTs_20230831} from './AaveV3_Polygon_SupplyCapLSTs_20230831.sol';
+
+/**
+ * @dev Test for AaveV3_Polygon_SupplyCapLSTs_20230831
+ * command: make test-contract filter=AaveV3_Polygon_SupplyCapLSTs_20230831
+ */
+contract AaveV3_Polygon_SupplyCapLSTs_20230831_Test is ProtocolV3TestBase {
+  AaveV3_Polygon_SupplyCapLSTs_20230831 internal proposal;
+
+  function setUp() public {
+    vm.createSelectFork(vm.rpcUrl('polygon'), 46981463);
+    proposal = new AaveV3_Polygon_SupplyCapLSTs_20230831();
+  }
+
+  function testProposalExecution() public {
+    ReserveConfig[] memory allConfigsBefore = createConfigurationSnapshot(
+      'preAaveV3_Polygon_SupplyCapLSTs_20230831',
+      AaveV3Polygon.POOL
+    );
+
+    GovHelpers.executePayload(vm, address(proposal), AaveGovernanceV2.POLYGON_BRIDGE_EXECUTOR);
+
+    ReserveConfig[] memory allConfigsAfter = createConfigurationSnapshot(
+      'postAaveV3_Polygon_SupplyCapLSTs_20230831',
+      AaveV3Polygon.POOL
+    );
+
+    diffReports(
+      'preAaveV3_Polygon_SupplyCapLSTs_20230831',
+      'postAaveV3_Polygon_SupplyCapLSTs_20230831'
+    );
+  }
+}
---
+++ src/20230831_AaveV3_Pol_SupplyCapLSTs/SupplyCapLSTs.md
@@ -0,0 +1,22 @@
+---
+title: "SupplyCapLSTs"
+author: "Alice Rozengarden (@Rozengarden - Aave-chan initiative)"
+discussions: "https://governance.aave.com/t/arfc-supply-cap-increase-lsts-on-polygon-v3/14696"
+---
+
+## Simple Summary
+
+## Motivation
+
+## Specification
+
+## References
+
+- Implementation: [Polygon](https://github.com/bgd-labs/aave-proposals/blob/main/src/20230831_AaveV3_Pol_SupplyCapLSTs/AaveV3_Polygon_SupplyCapLSTs_20230831.sol)
+- Tests: [Polygon](https://github.com/bgd-labs/aave-proposals/blob/main/src/20230831_AaveV3_Pol_SupplyCapLSTs/AaveV3_Polygon_SupplyCapLSTs_20230831.t.sol)
+- [Snapshot](No snapshot for Direct-to-AIP)
+- [Discussion](https://governance.aave.com/t/arfc-supply-cap-increase-lsts-on-polygon-v3/14696)
+
+## Copyright
+
+Copyright and related rights waived via [CC0](https://creativecommons.org/publicdomain/zero/1.0/).
